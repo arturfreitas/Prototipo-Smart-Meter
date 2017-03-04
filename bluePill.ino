2b40fcec2c37c6b0c97ea7bdd1d40da09ac2ad50
Artur Freitas <artur.freitas@ee.ufcg.edu.br>
2017-03-04 02:58:40 -0300

    Add files via upload
    Código inicial do projeto. Display funcionando.

---
+++ bluePill.ino
@@ -0,0 +1,18 @@
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(PB11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  Serial.println("Hello again");
+  digitalWrite(PB11, HIGH);
+  delay(500);
+  digitalWrite(PB11, LOW);
+  delay(500);
+}
